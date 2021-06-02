bdf8b255db9920fc746efa8ceef34358ad6f70de
Matt Baddour <mattbaddour@gmail.com>
2021-06-02 16:54:23 +1000

    added index, show, create pages

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,2 +1,4 @@
 class Flat < ApplicationRecord
+  validates :name, :description, :address, :number_of_guests, :price_per_night, presence: true
+  validates :number_of_guests, inclusion: { in: (1..20).to_a }, numericality: { only_integer: true }
 end
